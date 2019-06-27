dd7478ad9796fdccfbea85e0339feb8b9d4b83fc
ljh <18827453452@163.com>
2019-06-27 18:07:15 +0800

    Create calculator.py

---
+++ QQ_mail_auto_test/package/calculator.py
@@ -0,0 +1,12 @@
+class Count:
+    def __init__(self, a, b):
+        self.a = int(a)
+        self.b = int(b)
+
+    # 计算加法
+    def add(self):
+        return self.a + self.b
+
+    # 计算减法
+    def sub(self):
+        return self.a - self.b
