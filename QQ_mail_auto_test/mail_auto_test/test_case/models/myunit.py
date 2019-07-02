55905065bc7570a6ff0f143f051f534cd5a8d612
ljh <18827453452@163.com>
2019-07-02 17:59:24 +0800

    Create myunit.py

---
+++ QQ_mail_auto_test/mail_auto_test/test_case/models/myunit.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+import unittest
+
+
+class MyTest(unittest.TestCase):
+    """
+    unit测试框架
+    """
+
+    driver = webdriver.Firefox()
+
+    def setUp(self):
+        self.driver.implicitly_wait(10)
+        self.driver.maximize_window()
+
+    def tearDown(self):
+        self.driver.quit()
