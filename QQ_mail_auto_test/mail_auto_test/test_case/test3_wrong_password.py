73084e16e4860208f766bdf54c64ecde561e9149
ljh <18827453452@163.com>
2019-07-04 20:25:49 +0800

    Create test3_wrong_password.py

---
+++ QQ_mail_auto_test/mail_auto_test/test_case/test3_wrong_password.py
@@ -0,0 +1,28 @@
+import unittest
+from time import sleep
+import random
+import sys
+sys.path.append("./modles")
+sys.path.append("./page_obj")
+from page_obj.page_login import PageLogin
+from models.myunit import MyTest
+
+
+class TestWrongPassword(MyTest):
+    """登录测试"""
+
+    text = "你输入的帐号或密码不正确，请重新输入。"
+    num = str(random.randint(0, 199))
+
+    def test_wrong_password(self):
+        """错误密码登录测试"""
+
+        pl = PageLogin(self.driver)
+        pl.test_login(self.num + "1414710823@qq.com", self.num*3)
+        sleep(3)
+        assert(self.text == pl.login_error()), "你输入的帐号或密码不正确!"
+
+
+if __name__ == '__main__':
+    unittest.main()
+
