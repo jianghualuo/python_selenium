544f8ae2a189b485c16fd3e7abd5db69dd128c7d
ljh <18827453452@163.com>
2019-07-05 18:06:29 +0800

    Update test1_null_username.py

--- QQ_mail_auto_test/mail_auto_test/test_case/test1_null_username.py
+++ QQ_mail_auto_test/mail_auto_test/test_case/test1_null_username.py
@@ -1,5 +1,4 @@
 import unittest
-import random
 from time import sleep
 import sys
 sys.path.append("./models")
@@ -11,13 +10,10 @@ from page_obj.page_login import PageLogin
 class TestNullUsername(MyTest):
     """登录测试"""
 
-    text = "你还没有输入账号！"
-    num = str(random.randint(10000000, 99999999))
-
     def test_null_username(self):
-        """测试空户名和密码非空登录"""
+        """测试空户名和正确密码登录"""
         pl = PageLogin(self.driver)
-        pl.test_login("", self.num)
+        pl.test_login("", "LJH-ljh@169914@")
         sleep(3)
         self.assertEqual(pl.login_error(), "你还没有输入帐号！")
 
