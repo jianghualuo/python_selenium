351261386ad1bd360683ae21ede324cc3d569405
ljh <18827453452@163.com>
2019-07-05 18:07:02 +0800

    Update myunit.py

--- QQ_mail_auto_test/mail_auto_test/test_case/models/myunit.py
+++ QQ_mail_auto_test/mail_auto_test/test_case/models/myunit.py
@@ -1,15 +1,19 @@
 from selenium import webdriver
 import unittest
+from .driver import browser
 
 
 class MyTest(unittest.TestCase):
     """
     unit测试框架
     """
-
-    driver = webdriver.Firefox()
+    # 如下错误做法：在unittest的setUp初始化函数外定义驱动，导致第一个测试用例执行完毕环境被清除，之后没有再次定义驱动。
+    # driver = webdriver.Firefox()  # 错误1：
+    # driver = browser()            # 错误2：
 
     def setUp(self):
+        # self.driver = browser()
+        self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(10)
         self.driver.maximize_window()
 
