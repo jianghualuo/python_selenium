511ec77b5412f58524117d423b019471b6d80780
ljh <18827453452@163.com>
2019-07-05 18:06:24 +0800

    Update test2_null_password.py

--- QQ_mail_auto_test/mail_auto_test/test_case/test2_null_password.py
+++ QQ_mail_auto_test/mail_auto_test/test_case/test2_null_password.py
@@ -11,7 +11,6 @@ from page_obj.page_login import PageLogin
 class TestNullPassword(MyTest):
     """登录测试"""
 
-    text = "你还没有输入密码！"
     num = str(random.randint(0, 199))
 
     def test_null_password(self):
@@ -19,7 +18,7 @@ class TestNullPassword(MyTest):
         pl = PageLogin(self.driver)
         pl.test_login(self.num + "1414710823@qq.com", "")
         sleep(3)
-        assert(self.text == pl.login_error()), "你还没有输入密码!"
+        self.assertEqual(pl.login_error(), "你还没有输入密码！")
 
 
 if __name__ == '__main__':
