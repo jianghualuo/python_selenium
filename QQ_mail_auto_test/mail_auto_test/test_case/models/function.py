8bd2e5d162a2848c9e7915be5ccfaae396cfde38
ljh <18827453452@163.com>
2019-07-08 11:38:24 +0800

    Update function.py

--- QQ_mail_auto_test/mail_auto_test/test_case/models/function.py
+++ QQ_mail_auto_test/mail_auto_test/test_case/models/function.py
@@ -2,4 +2,12 @@ from selenium import webdriver
 
 
 def get_screenshot(driver, filename):
-    driver.getscreenshot_as_file(filename)
+    driver.get_screenshot_as_file(filename)
+
+
+# if __name__ == '__main__':
+#     driver = webdriver.Firefox()
+#     driver.get("https://www.baidu.com")
+#     driver.get_screenshot_as_file("./../../report/img/baidu.jpg")
+#     driver.quit()
+
