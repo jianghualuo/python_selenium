908612daa579ca8cbb14530f219a2a0a91cf3c4f
ljh <18827453452@163.com>
2019-07-09 18:06:41 +0800

    Update test5_next_time_auto_login.py

--- QQ_mail_auto_test/mail_auto_test/test_case/test5_next_time_auto_login.py
+++ QQ_mail_auto_test/mail_auto_test/test_case/test5_next_time_auto_login.py
@@ -7,21 +7,13 @@ sys.path.append("./models")
 from page_obj.page_login import PageLogin
 from models.myunit import MyTest
 from models.function import get_screenshot
+from selenium import webdriver
 
 
 class TestAutoLogin(MyTest):
     """测试“下次自动登录”功能"""
 
     text = '1414710823@qq.com'
-
-    def test0_login_ok(self):
-        """正确的用户名和密码登录测试"""
-        pl = PageLogin(self.driver)
-        pl.test_login()
-        self.driver.switch_to.default_content()
-        sleep(3)
-        assert(self.text == pl.login_success_hint()), '登录失败!'
-        print(self.driver.get_cookies())
 
     def test1_auto_login_next(self):
         """勾选【下次自动登录】登录测试"""
@@ -30,7 +22,6 @@ class TestAutoLogin(MyTest):
         sleep(3)
         self.driver.switch_to.default_content()
         assert(self.text == pl.login_success_hint()), '登录失败!'
-        print(self.driver.get_cookies())
 
     # def test1_auto_login(self):
     #     """验证启用“下次自动登录”是否生效"""
