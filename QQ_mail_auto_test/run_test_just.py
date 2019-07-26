2b7433d756bb449f9f66ace344f25b44c82e3f28
jianghualuo <18827453452@163.com>
2019-07-26 21:32:28 +0800

    Update run_test_just.py

--- QQ_mail_auto_test/run_test_just.py
+++ QQ_mail_auto_test/run_test_just.py
@@ -3,7 +3,7 @@ import time
 from HTMLTestRunner import HTMLTestRunner
 
 test_dir = "./mail_auto_test/test_case"
-discover = unittest.defaultTestLoader.discover(test_dir, pattern='test7*.py')
+discover = unittest.defaultTestLoader.discover(test_dir, pattern='test8*.py')
 
 
 if __name__ == '__main__':
